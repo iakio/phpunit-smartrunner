11b92cee9c73dd0a7d40eac61e66917e745a8ea3
ISHIDA Akio <ishida@coderium.com>
2015-08-23 17:35:18 +0900

    Refactor.

--- src/SmartRunner.php
+++ src/SmartRunner.php
@@ -4,21 +4,28 @@ namespace iakio\phpunit\smartrunner;
 
 class SmartRunner
 {
+    public function runCommand($file_name)
+    {
+        $fs = new FileSystem(getcwd());
+        $cache = new Cache($fs);
+        $cache->loadCache();
+        $arg_file = $fs->normalizePath($file_name);
+        $hits = $cache->get($arg_file);
+        if (count($hits) === 0 && self::isTestable($arg_file)) {
+            $hits = [$arg_file];
+        }
+        if (count($hits) > 0) {
+            $fs->saveSuiteFile($hits);
+            system(implode(DIRECTORY_SEPARATOR, ["vendor", "bin", "phpunit"]) . " SmartrunnerSuite .smartrunner/suite.php");
+        }
+    }
+
+
     public static function run(array $argv)
     {
+        $runner = new self;
         if (count($argv) > 1) {
-            $fs = new FileSystem(getcwd());
-            $cache = new Cache($fs);
-            $cache->loadCache();
-            $arg_file = $fs->normalizePath($argv[1]);
-            $hits = $cache->get($arg_file);
-            if (count($hits) === 0 && self::isTestable($arg_file)) {
-                $hits = [$arg_file];
-            }
-            if (count($hits) > 0) {
-                $fs->saveSuiteFile($hits);
-                system(implode(DIRECTORY_SEPARATOR, ["vendor", "bin", "phpunit"]) . " SmartrunnerSuite .smartrunner/suite.php");
-            }
+            $runner->runCommand($argv[1]);
         }
     }
 
